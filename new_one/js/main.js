df333e0b925dc511a0afce85999d3ab741b02ae8
Athulya Babu V K <58069723+athulyabvk@users.noreply.github.com>
2020-01-29 12:09:24 +0530

    Add files via upload

---
+++ new_one/js/main.js
@@ -0,0 +1,16 @@
+function verify(){  
+    var otp=document.verification.otp.value;  
+   if(isNaN(otp)){
+    alert("Enter Valid OTP");
+   }
+   else{
+    if (otp==null || otp==""){  
+        alert("Enter Your OTP here");  
+        return false;  
+      }else if(otp.length<6 || otp.length>6){  
+        alert("Enter Valid OTP");  
+        return false;  
+        }  
+   }
+    
+    }  
